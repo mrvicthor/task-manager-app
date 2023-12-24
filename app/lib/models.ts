ee0f5fe6cd0c467be969264018774a93371e2df6
Victor Eleanya <victoreleanya89@gmail.com>
2023-12-24 15:40:02 +0000

    lib/model.ts: added: added types for my data

---
+++ app/lib/models.ts
@@ -0,0 +1,25 @@
+export type Board = {
+  id?: number;
+  name: string;
+  columns: Column[];
+};
+
+export type Column = {
+  id?: number;
+  name: string;
+  tasks: Task[];
+};
+
+export type Task = {
+  id?: number;
+  title: string;
+  description?: string;
+  status: string;
+  subtasks: Subtask[];
+};
+
+export type Subtask = {
+  id?: number;
+  title: string;
+  isCompleted: boolean;
+};
