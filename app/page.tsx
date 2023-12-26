6e12ebdbe008331cba0fb5c096126f5f7a73735a
Victor Eleanya <victoreleanya89@gmail.com>
2023-12-26 15:26:11 +0000

    Sidebar.tsx: added: list of boards

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Image from "next/image";
+import { useAppSelector } from "./lib/hooks";
 
 export default function Home() {
+  const lightTheme = useAppSelector((state) => state.theme.lightTheme);
   return (
-    <main className="min-h-screen">
+    <main
+      className={`${lightTheme ? "bg-[#e4ebfa]" : "bg-[#20212c]"} min-h-screen`}
+    >
       <p>Task Manager</p>
       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,8 +1,15 @@
+"use client";
+import { useAppSelector } from "@/app/lib/hooks";
 import Image from "next/image";
 
 const Sidebar = () => {
+  const lighTheme = useAppSelector((state) => state.theme.lightTheme);
   return (
-    <section className="min-h-screen">
+    <section
+      className={`${
+        lighTheme ? "bg-[#ffffff]" : "bg-[#3e3f4e]"
+      } min-h-screen border-r border-[#e4ebfa]`}
+    >
       <div className="px-8 pt-4 flex gap-x-4 items-center">
         <div>
           <Image
@@ -16,6 +23,78 @@ const Sidebar = () => {
         <div>
           <p className="font-bold text-2xl">Kanban</p>
         </div>
+      </div>
+      <div className="mt-5">
+        <h2 className="px-8 pt-4 uppercase text-xs text-[#828fa3] font-bold">
+          all boards
+        </h2>
+        <ul className="mt-5 flex flex-col space-y-[1.8125rem]">
+          <li className="flex items-center gap-x-4 px-8">
+            <div>
+              <Image
+                src="./assets/icon-board.svg"
+                alt="board-icon"
+                height={16}
+                width={16}
+              />
+            </div>
+            <div>
+              <p className="font-bold capitalize text-[#828fa3] text-[15px]">
+                platform launch
+              </p>
+            </div>
+          </li>
+          <li className="flex items-center gap-x-4 px-8">
+            <div>
+              <Image
+                src="./assets/icon-board.svg"
+                alt="board-icon"
+                height={16}
+                width={16}
+              />
+            </div>
+            <div>
+              <p className="font-bold capitalize text-[#828fa3] text-[15px]">
+                platform launch
+              </p>
+            </div>
+          </li>{" "}
+          <li className="flex items-center gap-x-4 px-8">
+            <div>
+              <Image
+                src="./assets/icon-board.svg"
+                alt="board-icon"
+                height={16}
+                width={16}
+              />
+            </div>
+            <div>
+              <p className="font-bold capitalize text-[#828fa3] text-[15px]">
+                platform launch
+              </p>
+            </div>
+          </li>
+          <li className="flex items-center gap-x-4 px-8">
+            <div>
+              <Image
+                src="./assets/icon-board.svg"
+                alt="board-icon"
+                height={16}
+                width={16}
+              />
+            </div>
+            <div className="flex gap-x-[2px]">
+              <Image
+                src="./assets/icon-add-purple.svg"
+                alt="plus-icon"
+                width={5}
+                height={20}
+                className="self-center mt-1"
+              />
+              <p className="text-sm text-[#635fc7]">Create New Board</p>
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   );
