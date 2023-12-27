0dff8df1d20957a4ab8cb001fe58c15ef0b01107
Victor Eleanya <victoreleanya89@gmail.com>
2023-12-27 23:01:06 +0000

    seeded data

---
+++ prisma/migrations/20231227132342_init/migration.sql
@@ -0,0 +1,46 @@
+-- CreateTable
+CREATE TABLE "Board" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Board_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Column" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "boardId" INTEGER NOT NULL,
+
+    CONSTRAINT "Column_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" TEXT NOT NULL,
+    "columnId" INTEGER NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Subtask" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "isCompleted" BOOLEAN NOT NULL,
+    "taskId" INTEGER NOT NULL,
+
+    CONSTRAINT "Subtask_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Column" ADD CONSTRAINT "Column_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_columnId_fkey" FOREIGN KEY ("columnId") REFERENCES "Column"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Subtask" ADD CONSTRAINT "Subtask_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+import data from "../app/lib/data";
+
+const prisma = new PrismaClient();
+
+async function seed() {
+  for (const board of data.boards) {
+    const createdBoard = await prisma.board.create({
+      data: { name: board.name },
+    });
+
+    for (const column of board.columns) {
+      const createdColumn = await prisma.column.create({
+        data: { name: column.name, boardId: createdBoard.id },
+      });
+
+      for (const task of column.tasks) {
+        const createdTask = await prisma.task.create({
+          data: {
+            title: task.title,
+            description: task.description,
+            status: task.status,
+            columnId: createdColumn.id,
+          },
+        });
+
+        for (const subtask of task.subtasks) {
+          await prisma.subtask.create({
+            data: {
+              title: subtask.title,
+              isCompleted: subtask.isCompleted,
+              taskId: createdTask.id,
+            },
+          });
+        }
+      }
+    }
+  }
+}
+
+seed()
+  .catch((error) => {
+    throw error;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
