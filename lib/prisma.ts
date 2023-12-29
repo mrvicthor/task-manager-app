9c24022990eb30889a79f7e6a80ce6ea55c81895
Victor Eleanya <victoreleanya89@gmail.com>
2023-12-29 12:35:14 +0000

    prisma.ts: refactor: fix build issue

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,27 @@
+// import { PrismaClient } from "@prisma/client";
+// declare global {
+//   var prisma: PrismaClient;
+// }
+
+// // let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+// }
+
+// export default prisma;
+
 import { PrismaClient } from "@prisma/client";
+
 declare global {
-  var prisma: PrismaClient;
+  var prisma: PrismaClient | undefined;
 }
 
-// let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-}
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
