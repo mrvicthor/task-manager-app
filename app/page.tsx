e43076f92c6a3ec11c356e46fd1c4de529c14255
Victor Eleanya <victoreleanya89@gmail.com>
2023-12-30 08:27:46 +0000

    fix deployment error

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-import Image from "next/image";
-import { useAppSelector, useAppDispatch } from "../lib/hooks";
-// import { toggleSidebar } from "../lib/features/sidebar/sidebarSlice";
 import { Boards, Toggle, MainScreen } from "@/components";
 
 export default function Home() {
-  // const dispatch = useAppDispatch();
-
-  // const handleSidebar = () => dispatch(toggleSidebar());
   return (
     <MainScreen>
       <Boards />
--- components/Toggle/index.tsx
+++ components/Toggle/index.tsx
@@ -9,7 +9,7 @@ export const Toggle = () => {
   const handleSidebar = () => dispatch(toggleSidebar());
   return (
     <>
-      {!showSidebar && (
+      {showSidebar ? null : (
         <div
           onClick={handleSidebar}
           className="bg-[#635fc7] h-[2.4rem] w-[56px] md:flex items-center justify-center fixed bottom-24 rounded-r-full cursor-pointer hidden z-20"
