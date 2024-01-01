24f79718a33506f9a1e5881f7bf0bd24a62e3009
Victor Eleanya <victoreleanya89@gmail.com>
2024-01-01 21:08:22 +0000

    change api/boards function name to handle

--- app/api/boards/route.ts
+++ app/api/boards/route.ts
@@ -1,7 +1,7 @@
 import prisma from "../../../lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function handle() {
   const data = await prisma.board.findMany();
   return NextResponse.json(data);
 }
