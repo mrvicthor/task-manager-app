b25078f8d70312ebfe776163dbb79750e2f1cae6
Victor Eleanya <victoreleanya89@gmail.com>
2024-01-01 20:41:04 +0000

    fixed deployment error

--- components/Boards/index.tsx
+++ components/Boards/index.tsx
@@ -1,17 +1,9 @@
-// import prisma from "@/app/lib/prisma";
+import React from "react";
 import { Board } from "@prisma/client";
-const getBoards = async () => {
-  const res = await fetch("http://localhost:3000/api/boards", {
-    next: { revalidate: 3600 },
-  });
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
-  return res.json();
-};
+import prisma from "@/lib/prisma";
 
 const Boards = async () => {
-  const boards = await getBoards();
+  const boards = await prisma.board.findMany();
 
   return (
     <section>
