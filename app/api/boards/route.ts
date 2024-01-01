d8cc58d05feeffff41613d0ab0d97d4c5e6ab60c
Victor Eleanya <victoreleanya89@gmail.com>
2024-01-01 21:14:46 +0000

    deleted boards api

--- app/api/boards/route.ts
+++
@@ -1,7 +0,0 @@
-import prisma from "../../../lib/prisma";
-import { NextResponse } from "next/server";
-
-export async function handle() {
-  const data = await prisma.board.findMany();
-  return NextResponse.json(data);
-}
