ce19dbb1a28a9143faf497dc110c41dd53020b89
Victor Eleanya <victoreleanya89@gmail.com>
2024-01-23 23:11:13 +0000

    updated text content

--- components/Columns/index.tsx
+++ components/Columns/index.tsx
@@ -19,9 +19,13 @@ const Columns = () => {
       <h1 className="text-center capitalize font-bold text-2xl">
         welcome to the kanban task manager app
       </h1>
-      <p className="text-center ">
-        <span className="animate-ping text-lg">👈</span> open the sidebar to add
-        or navigate to board
+      <p className="text-center hidden md:block capitalize">
+        <span className=" animate-ping text-lg">👈</span> open the sidebar to
+        add board or navigate between boards
+      </p>
+      <p className="text-center md:hidden capitalize">
+        <span className="animate-bounce text-lg">👆</span> click on the drop
+        down to add board or navigate between boards
       </p>
     </section>
   );
