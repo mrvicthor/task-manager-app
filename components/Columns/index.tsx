260f1a9a14fb89b1293fd3f2e13d55ce1b953eaf
Victor Eleanya <victoreleanya89@gmail.com>
2024-01-23 21:10:37 +0000

    added welcome page

--- components/Columns/index.tsx
+++ components/Columns/index.tsx
@@ -1,7 +1,28 @@
+"use client";
+import { useAppSelector } from "@/lib/hooks";
+import { Player } from "@lottiefiles/react-lottie-player";
+
 const Columns = () => {
+  const lightTheme = useAppSelector((state) => state.theme.lightTheme);
   return (
-    <section>
-      <p>Columns</p>
+    <section
+      className={`${
+        lightTheme ? "bg-[#F4F7FD] text-[#000112]" : "bg-[#20212c] text-white"
+      }`}
+    >
+      <Player
+        autoplay
+        loop
+        src="https://lottie.host/a65d390a-e530-47ff-91c0-d7290e8845a2/uidMOKoai4.json"
+        style={{ height: "450px", width: "350px" }}
+      ></Player>
+      <h1 className="text-center capitalize font-bold text-2xl">
+        welcome to the kanban task manager app
+      </h1>
+      <p className="text-center ">
+        <span className="animate-ping text-lg">👈</span> open the sidebar to add
+        or navigate to board
+      </p>
     </section>
   );
 };
