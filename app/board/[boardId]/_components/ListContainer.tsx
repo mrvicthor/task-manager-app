8e613ddcfdadea4a07f7b4bbc4d137570afed964
Victor Eleanya <victoreleanya89@gmail.com>
2024-02-19 21:20:33 +0000

    ListContainer.tsx: fix: drag and drop logic

--- app/board/[boardId]/_components/ListContainer.tsx
+++ app/board/[boardId]/_components/ListContainer.tsx
@@ -34,6 +34,7 @@ const ListContainer = ({ board, columns, subtasks }: DetailsProps) => {
   // const data = useAppSelector((state) => state.board.columns);
   const [data, setData] = useState<Column[]>(columns);
   const [tasks, setTasks] = useState<Task[]>([]);
+  // const [isLoading, setIsLoading] = useState(true);
   // const todos = useAppSelector((state) => state.board.tasks);
   const dispatch = useAppDispatch();
 
@@ -46,43 +47,25 @@ const ListContainer = ({ board, columns, subtasks }: DetailsProps) => {
   const onDragEnd = (result: DropResult) => {
     const { destination, source, type } = result;
     if (!destination) return;
-
-    // handle drag events for columns
-    if (type === "column") {
-      const entries = Array.from(data);
-      const [removed] = entries.splice(source.index, 1);
-      entries.splice(destination.index, 0, removed);
-      const reorderedColumns = entries;
-      setData(reorderedColumns);
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
     }
 
-    const columns = Array.from(data);
-    const startColumnIndex = columns.find(
-      (column) => column.name === source.droppableId
-    )?.id;
-    const finishColumnIndex = columns.find(
-      (column) => column.name === destination.droppableId
-    )?.id;
-
-    if (!startColumnIndex || !finishColumnIndex) return;
-    if (
-      source.index === destination.index &&
-      startColumnIndex === finishColumnIndex
-    )
-      return;
-
-    const startColumn = data[startColumnIndex - 1];
-    const finishColumn = data[finishColumnIndex - 1];
-    if (startColumn === finishColumn) {
-      // within the same column
-      const newTasks = Array.from(startColumn.tasks);
+    console.log(source.droppableId, destination.droppableId);
+    const start = data[Number(source.droppableId)];
+    const finish = data[Number(destination.droppableId)];
+    console.log(start, finish);
+    if (start === finish) {
+      const newTasks = Array.from(start.tasks);
       const [movedTask] = newTasks.splice(source.index, 1);
       newTasks.splice(destination.index, 0, movedTask);
       const newColumn = {
-        ...startColumn,
+        ...start,
         tasks: newTasks,
       };
-
       setData(
         data.map((column) =>
           column.id === newColumn.id
@@ -90,75 +73,67 @@ const ListContainer = ({ board, columns, subtasks }: DetailsProps) => {
             : column
         )
       );
-    } else {
-      const startTask = Array.from(startColumn.tasks);
-      const [movedTask] = startTask.splice(source.index, 1);
-      const newStartColumn = {
-        ...startColumn,
-        tasks: startTask,
-      };
-      const newFinishTask = Array.from(finishColumn.tasks);
-      newFinishTask.splice(destination.index, 0, {
-        ...movedTask,
-        columnId: finishColumn.id,
-        status: finishColumn.name,
-      });
-      const newFinishColumn = {
-        ...finishColumn,
-        tasks: newFinishTask,
-      };
-
-      setData((prevData) =>
-        prevData.map((column) => {
-          if (column.id === newStartColumn.id) {
-            return { ...column, tasks: newStartColumn.tasks };
-          } else if (column.id === newFinishColumn.id) {
-            return { ...column, tasks: newFinishColumn.tasks };
-          } else {
-            return column;
-          }
-        })
-      );
+      return;
     }
+
+    // Moving from one list to another column
+    const startTasks = Array.from(start.tasks);
+    const [movedTask] = startTasks.splice(source.index, 1);
+    const newTasks = {
+      ...start,
+      tasks: startTasks,
+    };
+    const finishTasks = Array.from(finish.tasks);
+    finishTasks.splice(destination.index, 0, {
+      ...movedTask,
+      columnId: finish.id,
+      status: finish.name,
+    });
+    const newFinish = {
+      ...finish,
+      tasks: finishTasks,
+    };
+
+    setData((prevData) =>
+      prevData.map((column) => {
+        if (column.id === newTasks.id) {
+          return { ...column, tasks: newTasks.tasks };
+        } else if (column.id === newFinish.id) {
+          return { ...column, tasks: newFinish.tasks };
+        } else {
+          return column;
+        }
+      })
+    );
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="lists" type="column" direction="horizontal">
-        {(provided) => (
-          <ol
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-            className={`${
-              lightTheme
-                ? "bg-[#F4F7FD] text-[#000112]"
-                : "bg-[#20212c] text-white"
-            }  ${
-              showSidebar ? "md:translate-x-[18.75rem]" : "translate-x-0"
-            } sidebar h-[100vh] min-w-[1440px] mt-16 overflow-x-auto flex gap-6 pl-6`}
-          >
-            <Suspense fallback={<p>Loading columns</p>}>
-              <div className="gap-6 pt-6 flex">
-                {data.map((column, index) => {
-                  return (
-                    <Column
-                      key={column.id}
-                      id={column.id}
-                      status={column.name}
-                      data={column.tasks}
-                      subtask={subtasks}
-                      index={index}
-                    />
-                  );
-                })}
-              </div>
-
-              {provided.placeholder}
-            </Suspense>
-            <NewColumnClient />
-          </ol>
-        )}
-      </Droppable>
+      <ol
+        className={`${
+          lightTheme ? "bg-[#F4F7FD] text-[#000112]" : "bg-[#20212c] text-white"
+        }  ${
+          showSidebar ? "md:translate-x-[18.75rem]" : "translate-x-0"
+        } sidebar h-[100vh] min-w-[1440px] mt-16 overflow-x-auto flex gap-6 pl-6`}
+      >
+        <Suspense fallback={<p>Loading columns</p>}>
+          <div className="gap-6 pt-6 flex">
+            {data.map((column, index) => {
+              return (
+                <Column
+                  key={column.id}
+                  id={column.id}
+                  status={column.name}
+                  data={column.tasks}
+                  subtask={subtasks}
+                  index={index}
+                />
+              );
+            })}
+          </div>
+        </Suspense>
+        <NewColumnClient />
+      </ol>
     </DragDropContext>
   );
 };
