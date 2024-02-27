46eaabd7562c64111a32711e2fb4d850ec5dc240
Victor Eleanya <victoreleanya89@gmail.com>
2024-02-27 22:09:35 +0000

    Form.tsx: update: style

--- components/Form/index.tsx
+++ components/Form/index.tsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import { Select, Button } from "..";
 import Image from "next/image";
+import { useAppSelector } from "@/lib/hooks";
+
 type IStatus = "Todo" | "Doing" | "Done";
 type Option = {
   id: number;
@@ -10,6 +12,7 @@ type Option = {
 const Form = () => {
   const [showOptions, setShowOptions] = useState(false);
   const [value, setValue] = useState<Option | null>(null);
+  const lighTheme = useAppSelector((state) => state.theme.lightTheme);
   const options = [
     { id: 1, title: "Todo" },
     { id: 2, title: "Doing" },
@@ -21,32 +24,48 @@ const Form = () => {
   };
 
   return (
-    <form className="flex flex-col gap-6 mt-6">
+    <form className="flex flex-col gap-5 mt-5">
       <div>
-        <label className="text-[#828FA3] capitalize" htmlFor="title">
+        <label
+          className={`${
+            lighTheme ? "text-[#828FA3]" : "text-white"
+          }  capitalize`}
+          htmlFor="title"
+        >
           title
         </label>
         <input
-          className="w-full py-2 px-5 inline-block border border-[#ccc] rounded"
+          className={`w-full py-2 px-5 inline-block border border-[#ccc] border-opacity-50 placeholder:opacity-50 rounded mt-1 bg-transparent`}
           id="title"
           placeholder="e.g Take coffee break"
         />
       </div>
       <div>
-        <label className="text-[#828FA3] capitalize" htmlFor="description">
+        <label
+          className={`${
+            lighTheme ? "text-[#828FA3]" : "text-white"
+          } capitalize`}
+          htmlFor="description"
+        >
           description
         </label>
         <textarea
           id="description"
-          className="w-full py-3 px-5 inline-block border border-[#ccc] rounded resize-none h-[112px]"
+          className={`w-full py-3 px-5 inline-block border border-[#ccc] border-opacity-50 placeholder:opacity-50 rounded resize-none h-[112px] mt-1 bg-transparent`}
           placeholder="e.g It's always good to take a break. This 15 minutes break will recharge the battery a little."
         ></textarea>
       </div>
       <div>
-        <label className="text-[#828FA3] capitalize">subtask</label>
-        <div className="flex gap-4">
+        <label
+          className={`${
+            lighTheme ? "text-[#828FA3]" : "text-white"
+          } capitalize`}
+        >
+          subtask
+        </label>
+        <div className="flex gap-4 mt-1">
           <input
-            className="w-full py-2 px-5 inline-block border border-[#ccc] rounded"
+            className={`w-full py-2 px-5 inline-block border border-[#ccc] border-opacity-50 placeholder:opacity-50 rounded bg-transparent`}
             placeholder="e.g Make coffee"
           />{" "}
           <div className="flex items-center">
@@ -60,7 +79,7 @@ const Form = () => {
         </div>
         <div className="flex gap-4 mt-4">
           <input
-            className="w-full py-2 px-5 inline-block border border-[#ccc] rounded"
+            className={`w-full py-2 px-5 inline-block border border-[#ccc] border-opacity-50 placeholder:opacity-50 rounded bg-transparent`}
             placeholder="e.g Drink coffee and smile"
           />
           <div className="flex items-center">
@@ -73,27 +92,32 @@ const Form = () => {
           </div>
         </div>
         <Button
-          style="flex gap-x-1 bg-[#e4ebfa] w-full h-[40px] rounded-full items-center justify-center"
+          style="flex gap-x-1 bg-[#e4ebfa] w-full h-[40px] rounded-full items-center justify-center capitalize mt-2"
           onClick={() => console.log("clicked")}
         >
           <Image
-            src={"/" + "./assets/icon-add-task-mobile.svg"}
+            src={"/" + "./assets/icon-add-purple.svg"}
             alt="plus-icon"
             width={5}
             height={20}
             className="self-center mt-1"
           />
-          <p className="text-sm">add new subtask</p>
+          <p className="text-sm text-[#635fc7]">add new subtask</p>
         </Button>
       </div>
       <div>
-        <label htmlFor="status" className="text-[#828FA3] capitalize">
+        <label
+          htmlFor="status"
+          className={`${
+            lighTheme ? "text-[#828FA3]" : "text-white"
+          } capitalize`}
+        >
           status
         </label>
         <Select options={options} value={value} onChange={onChange} />
       </div>
       <Button
-        style="bg-[#635fc7] w-full h-[40px] rounded-full"
+        style="bg-[#635fc7] w-full h-[40px] rounded-full capitalize text-white"
         onClick={() => console.log("clicked")}
       >
         create task
--- components/Select/index.tsx
+++ components/Select/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import Image from "next/image";
+import { useAppSelector } from "@/lib/hooks";
 
 type Option = {
   id: number;
@@ -14,14 +15,17 @@ type SelectProps = {
 };
 const Select = ({ value, onChange, options }: SelectProps) => {
   const [showOptions, setShowOptions] = useState(false);
+  const lighTheme = useAppSelector((state) => state.theme.lightTheme);
 
   return (
     <div
       tabIndex={0}
       onClick={() => setShowOptions(!showOptions)}
-      className="w-full h-[42px] px-5 flex items-center border border-[#ccc] rounded relative"
+      className="w-full h-[42px] px-5 flex items-center border border-[#ccc] border-opacity-50 rounded relative mt-1"
     >
-      <span className="">{value?.title ? value.title : "Todo"}</span>
+      <span className={`${lighTheme ? "text-black" : "text-white"}`}>
+        {value?.title ? value.title : "Todo"}
+      </span>
       <div className="absolute right-4 top-4">
         <Image
           src={`/${
@@ -35,10 +39,14 @@ const Select = ({ value, onChange, options }: SelectProps) => {
         />
       </div>
       {showOptions && (
-        <ul className="absolute top-[45px] left-0 right-0 border divide-y rounded">
+        <ul
+          className={`${
+            lighTheme ? "bg-white" : "bg-[#20212c]"
+          } absolute top-[45px] left-0 right-0 rounded z-[30000px]`}
+        >
           {options.map((item) => (
             <li
-              className={`py-1 px-4 cursor-pointer hover:text-[#828FA3]`}
+              className={`py-1 px-4 cursor-pointer text-[#828FA3]`}
               key={item.id}
               value={item.title}
               onClick={() => onChange(item)}
