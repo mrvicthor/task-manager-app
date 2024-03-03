debd4075e0d16e4b22c180350a0db6564f084b9b
Victor Eleanya <victoreleanya89@gmail.com>
2024-03-03 20:10:58 +0000

    form.tsx: added: usefieldarray hook

--- components/Form/index.tsx
+++ components/Form/index.tsx
@@ -2,24 +2,33 @@
 import { Select, Button } from "..";
 import Image from "next/image";
 import { useAppSelector } from "@/lib/hooks";
-import { useForm } from "react-hook-form";
-// import { z } from "zod";
+import { useForm, useFieldArray } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { schema } from "@/lib/formScema";
+import { z } from "zod";
 
 type IStatus = "Todo" | "Doing" | "Done";
-type ISubtasks = string;
-type FormData = {
-  title: string;
-  description?: string;
-  subtasks?: ISubtasks[];
-  status: IStatus;
-};
+
 type Option = {
   id: number;
   title: string;
 };
 
 const Form = () => {
-  const { handleSubmit, register, control } = useForm<FormData>();
+  const {
+    handleSubmit,
+    register,
+    control,
+
+    formState: { errors, isSubmitting },
+  } = useForm<z.output<typeof schema>>({
+    resolver: zodResolver(schema),
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "subtasks",
+  });
   const lighTheme = useAppSelector((state) => state.theme.lightTheme);
   const options = [
     { id: 1, title: "Todo" },
@@ -27,7 +36,7 @@ const Form = () => {
     { id: 3, title: "Done" },
   ];
 
-  const onSubmit = (data: FormData) => console.log(data);
+  const onSubmit = (data: any) => console.log(data);
 
   return (
     <form
@@ -50,6 +59,7 @@ const Form = () => {
           placeholder="e.g Take coffee break"
         />
       </div>
+      {errors.title && <small>{errors.title.message}</small>}
       <div>
         <label
           className={`${
@@ -74,39 +84,29 @@ const Form = () => {
         >
           subtask
         </label>
-        <div className="flex gap-4 mt-1">
-          <input
-            {...register("subtasks")}
-            className={`w-full py-2 px-5 inline-block border border-[#ccc] border-opacity-50 placeholder:opacity-50 rounded bg-transparent`}
-            placeholder="e.g Make coffee"
-          />{" "}
-          <div className="flex items-center">
-            <Image
-              src={"/" + "./assets/icon-cross.svg"}
-              alt="cross-icon"
-              height={16}
-              width={16}
-            />
+
+        {fields.map((field, index) => (
+          <div className="flex gap-4 mt-1" key={field.id}>
+            <input
+              {...register(`subtasks.${index}.title` as const)}
+              className={`w-full py-2 px-5 inline-block border border-[#ccc] border-opacity-50 placeholder:opacity-50 rounded bg-transparent`}
+              placeholder="e.g Drink coffee and smile"
+            />{" "}
+            <div onClick={() => remove(index)} className="flex items-center">
+              <Image
+                src={"/" + "./assets/icon-cross.svg"}
+                alt="cross-icon"
+                height={16}
+                width={16}
+              />
+            </div>
           </div>
-        </div>
-        <div className="flex gap-4 mt-4">
-          <input
-            {...register("subtasks")}
-            className={`w-full py-2 px-5 inline-block border border-[#ccc] border-opacity-50 placeholder:opacity-50 rounded bg-transparent`}
-            placeholder="e.g Drink coffee and smile"
-          />
-          <div className="flex items-center">
-            <Image
-              src={"/" + "./assets/icon-cross.svg"}
-              alt="cross-icon"
-              height={16}
-              width={16}
-            />
-          </div>
-        </div>
-        <Button
-          style="flex gap-x-1 bg-[#e4ebfa] w-full h-[40px] rounded-full items-center justify-center capitalize mt-2"
-          onClick={() => console.log("clicked")}
+        ))}
+
+        <button
+          type="button"
+          className="flex gap-x-1 bg-[#e4ebfa] w-full h-[40px] rounded-full items-center justify-center capitalize mt-2"
+          onClick={() => append({ title: "", isCompleted: false })}
         >
           <Image
             src={"/" + "./assets/icon-add-purple.svg"}
@@ -116,7 +116,7 @@ const Form = () => {
             className="self-center mt-1"
           />
           <p className="text-sm text-[#635fc7]">add new subtask</p>
-        </Button>
+        </button>
       </div>
       <div>
         <label
@@ -129,9 +129,12 @@ const Form = () => {
         </label>
         <Select options={options} control={control} name="status" />
       </div>
-      <Button style="bg-[#635fc7] w-full h-[40px] rounded-full capitalize text-white">
+      <button
+        type="submit"
+        className="bg-[#635fc7] w-full h-[40px] rounded-full capitalize text-white"
+      >
         create task
-      </Button>
+      </button>
     </form>
   );
 };
---
+++ lib/formScema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const Subtask = z.object({
+  title: z.string(),
+  isCompleted: z.boolean(),
+});
+
+export const schema = z.object({
+  title: z.string().trim().min(3, { message: "title is required" }),
+  description: z.optional(z.string()),
+  subtasks: z.optional(z.array(Subtask)),
+  status: z.enum(["Todo", "Doing", "Done"]),
+});
